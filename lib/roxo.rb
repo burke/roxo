0075fa19cdaa28f489daef21a7bace867a810540
Burke Libbey <burke@burkelibbey.org>
2010-04-22 10:02:02 -0500

    various fixes.

--- lib/roxo.rb
+++ lib/roxo.rb
@@ -8,13 +8,15 @@ class ROXO
   include Comparable
   
   attr_reader :raw, :name, :value, :children
-  def __attributes; @attributes.sort;       end
   def   attributes; @attributes.keys;       end
-  def    terminal?; @children.size.zero?;   end # A node is terminal if we can't descend any further
   def      has?(o); !! send(o);             end # Boolean. Does this exist?
   def        [](o); @attributes[o.to_s];    end # Only lookup attributes
   def         to_s; @value;                 end 
   
+  def __attributes
+    @attributes.sort.reject{|a|a.to_s =~ /^xmlns/}
+  end 
+
   def initialize(xml)
     xml = ::LibXML::XML::Parser.string(xml).parse.root unless xml.kind_of?(LibXML::XML::Node)
 
@@ -27,14 +29,25 @@ class ROXO
     terminal? ? value.inspect : "#<ROXO:0x#{object_id.to_s(16)} @name=\"#{@name}\", ...>"
   end 
 
+  def terminal? 
+    @children.size.zero? and @attributes.size.zero?
+  end # A node is terminal if we can't descend any further
+
   def <=>(other)
-    [:class, :__attributes, :value, :name].each do |key|
-      return z unless (z = self.send(key) <=> other.send(key)).zero?
-    end 
+    return z unless (z = self.class <=> other.class).zero?
+    return z unless (z = self.__attributes <=> other.__attributes).zero?
+    return z unless (z = self.value <=> other.value).zero?
+    return z unless (z = self.name <=> other.name).zero?
+
     nodes = lambda{|obj|obj.children.values.flatten.map{|n|self.class.new(n)}}
     nodes[self] <=> nodes[other]
   end
 
+  def ==(o)
+    self.<=>(o) == 0
+  end 
+
+  
   def method_missing(sym, *args)
     if terminal? # Proxy all method calls to the wrapped object.
       return @value.send(sym, *args)
